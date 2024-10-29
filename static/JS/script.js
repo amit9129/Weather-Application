748bfbede65100a8a40bf161de1e66ede99eebc6
amit9129 <Kumaramit.14singh@gmail.com>
2024-10-29 10:10:23 +0530

    done changes

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,47 +1,45 @@
-$(document).ready(function() {
-    $('#get-weather-btn').click(function() {
+$(document).ready(function () {
+    $('#get-weather-btn').click(function () {
         var city = $('#city-input').val().trim();
+        $('#weather-alert').hide(); // Hide any previous alerts
+
         if (city) {
-            $.get('/weather', { city: city }, function(data) {
+            $.get('/weather', { city: city }, function (data) {
                 if (data.error) {
-                    alert(data.error);
+                    $('#weather-alert').text(data.error).fadeIn();
                 } else {
-                    // Display main weather info
+                    // Display weather information
                     $('#city-name').text(data.city);
                     $('#temperature').text(data.current_weather.temperature);
                     $('#weather-description').text(data.current_weather.description);
-                    $('#weather-info').show();
+                    $('#weather-info').fadeIn();
 
                     // Clear previous forecast data
                     $('#daily-forecast-cards').empty();
 
                     // Display 5-day forecast
-                    data.daily_forecasts.forEach(function(forecast) {
+                    data.daily_forecasts.forEach(function (forecast) {
                         var forecastCard = `
-                            <div class="col-md-2">
-                                <div class="card forecast">
-                                    <div class="card-body">
-                                        <h6>${forecast.date}</h6>
-                                        <p>Min: ${forecast.min_temp} °C</p>
-                                        <p>Max: ${forecast.max_temp} °C</p>
-                                        <p>${forecast.description}</p>
-                                    </div>
-                                </div>
+                            <div class="col-md-2 forecast">
+                                <h6>${forecast.date}</h6>
+                                <p>Min: ${forecast.min_temp} °C</p>
+                                <p>Max: ${forecast.max_temp} °C</p>
+                                <p>${forecast.description}</p>
                             </div>
                         `;
                         $('#daily-forecast-cards').append(forecastCard);
                     });
 
-                    $('#daily-forecast-section').show();
+                    $('#daily-forecast-section').fadeIn();
 
-                    // Change background GIF based on weather
+                    // Change background GIF based on current weather
                     changeBackground(data.current_weather.weather);
                 }
-            }).fail(function() {
-                alert("Error fetching weather data.");
+            }).fail(function () {
+                $('#weather-alert').text("Error fetching weather data. Please try again.").fadeIn();
             });
         } else {
-            alert("Please enter a city name.");
+            $('#weather-alert').text("Please enter a city name.").fadeIn();
         }
     });
 
@@ -50,7 +48,7 @@ $(document).ready(function() {
         container.innerHTML = ''; // Clear previous content
 
         switch (weather) {
-            case 'haze':
+            case 'clear':
                 container.innerHTML = `<div class="tenor-gif-embed" data-postid="7525721051952338516" data-share-method="host" data-aspect-ratio="1" data-width="100%">
                 <a href="https://tenor.com/view/good-morning-good-morning-love-good-morning-my-love-morning-love-gm-gif-7525721051952338516">Good Morning GIF</a> 
                 from <a href="https://tenor.com/search/good+morning-gifs">Good Morning GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>`;
@@ -62,18 +60,13 @@ $(document).ready(function() {
                 <a href="https://tenor.com/search/dying-gifs">Dying GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>`;
                 break;
 
-            case 'clouds':
-                container.innerHTML = `<div class="tenor-gif-embed" data-postid="10012944" data-share-method="host" data-aspect-ratio="1" data-width="100%">
-                <a href="https://tenor.com/view/clouds-cloudy-sky-sky-gif-10012944">Cloudy GIF</a> from 
-                <a href="https://tenor.com/search/clouds-gifs">Clouds GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>`;
-                break;
-
             case 'night':
             case 'mist':
                 container.innerHTML = `<div class="tenor-gif-embed" data-postid="9167088073175345749" data-share-method="host" data-aspect-ratio="0.982143" data-width="100%">
                 <a href="https://tenor.com/view/good-night-starry-night-sleep-well-gif-9167088073175345749">Good Night GIF</a> from 
                 <a href="https://tenor.com/search/good+night-gifs">Good Night GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>`;
                 break;
+
             default:
                 container.innerHTML = `<p>No GIF available for this weather condition.</p>`;
                 break;
